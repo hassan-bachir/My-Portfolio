118f829767289be9a77ce5328b780991ad5c99d9
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-08-12 15:50:33 +0300

    updated index.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-
-function App() {
-    const [count, setCount] = useState(0);
-
-    return <></>;
-}
+const App = () => {
+    return (
+        <>
+            <div>3D developer portfolio!</div>
+        </>
+    );
+};
 
 export default App;
