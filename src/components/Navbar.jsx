21c2f21b08a2806d4941a829d7236ecbecaf6cec
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-08-16 16:43:16 +0300

    added elements to navigation bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -26,10 +26,17 @@ const Navbar = () => {
                         className="w-9 h-9 object-contain "
                     />
                     <p className="text-white text-[18px] font-bold cursor-pointer">
-                        Hasan{" "}
-                        <span className="sm:block hidden">| Portfolio </span>{" "}
+                        Hasan
+                        <span className="sm:block hidden">| Portfolio </span>
                     </p>
                 </Link>
+                <ul className="list-none hidden sm:flex flex-row gap-10">
+                    {navLinks.map((link) => (
+                        <li>
+                            <a href={`#${link.id}`}>{link.title}</a>
+                        </li>
+                    ))}
+                </ul>
             </div>
         </nav>
     );
