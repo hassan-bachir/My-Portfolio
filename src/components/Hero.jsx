ae13522945d3ce6a9336ec6aa889aaf887353dd2
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-08-18 15:16:55 +0300

    created a new div

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,13 @@ import { styles } from "../styles";
 import { ComputersCanvas } from "./canvas";
 
 const Hero = () => {
-    return <section className="relative w-full h-screen mx-auto">Hero</section>;
+    return (
+        <section className="relative w-full h-screen mx-auto">
+            <div
+                className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
+            ></div>
+        </section>
+    );
 };
 
 export default Hero;
