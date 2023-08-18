aff2a847d0b1cbc1143c417c6b427ea05e7d0946
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-08-18 16:30:45 +0300

    added the title with styling

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,6 +13,12 @@ const Hero = () => {
                     <div className="w-5 h-5 rounded-full bg-[#915eff]" />
                     <div className="w-1 sm:h-80 h-40 violet-gradient " />
                 </div>
+                <div>
+                    <h1>
+                        Hi, I'm
+                        <span className="text-[#915eff]"> Hasan </span>
+                    </h1>
+                </div>
             </div>
         </section>
     );
