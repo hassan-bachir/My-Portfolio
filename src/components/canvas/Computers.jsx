a152ddd744734cbaf9ca83b478ff716845e66f61
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-08-19 22:10:25 +0300

    fixed the lights on the model

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -8,9 +8,14 @@ const Computers = () => {
 
     return (
         <mesh>
-            <hemisphereLight intensity={0.15} groundColor="black" />
+            <hemisphereLight intensity={1} />
             <pointLight intensity={1} />
-            <primitive object={Computer.scene} />
+            <primitive
+                object={Computer.scene}
+                scale={0.75}
+                position={[0, -3.25, -1.5]}
+                rotation={[-0.01, -0.2, -0.1]}
+            />
         </mesh>
     );
 };
@@ -22,7 +27,7 @@ const ComputersCanvas = () => {
             camera={{ position: [20, 3, 5], fov: 25 }}
             gl={{ preserveDrawingBuffer: true }}
         >
-            <Suspense fallback={<CanvasLoader />}>
+            <Suspense>
                 <OrbitControls
                     enableZoom={false}
                     maxPolarAngle={Math.PI / 2}
@@ -34,4 +39,4 @@ const ComputersCanvas = () => {
         </Canvas>
     );
 };
-export default Computers;
+export default ComputersCanvas;
