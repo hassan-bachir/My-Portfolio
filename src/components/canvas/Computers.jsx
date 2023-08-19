bb07a7a0fba7442c548c3d4ac84ad53e19573c99
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-08-19 21:53:29 +0300

    fixed an error that gave 4 other erros :)

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -5,7 +5,33 @@ import CanvasLoader from "../Loader";
 
 const Computers = () => {
     const Computer = useGLTF("./desktop_pc/scene.gltf");
-    return <div>Computers</div>;
-};
 
+    return (
+        <mesh>
+            <hemisphereLight intensity={0.15} groundColor="black" />
+            <pointLight intensity={1} />
+            <primitive object={Computer.scene} />
+        </mesh>
+    );
+};
+const ComputersCanvas = () => {
+    return (
+        <Canvas
+            frameloop="demand"
+            shadows
+            camera={{ position: [20, 3, 5], fov: 25 }}
+            gl={{ preserveDrawingBuffer: true }}
+        >
+            <Suspense fallback={<CanvasLoader />}>
+                <OrbitControls
+                    enableZoom={false}
+                    maxPolarAngle={Math.PI / 2}
+                    minPolarAngle={Math.PI / 2}
+                />
+                <Computers />
+            </Suspense>
+            <Preload all />
+        </Canvas>
+    );
+};
 export default Computers;
