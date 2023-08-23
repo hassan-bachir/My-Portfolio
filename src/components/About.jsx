a6f2dae63dafb4d28e00d494d6bbfa11efe99ad3
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-08-23 14:53:02 +0300

    added bio

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,7 +7,22 @@ import { services } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
 
 const About = () => {
-    return <div>About</div>;
+    return (
+        <>
+            <motion.div variants={textVariant()}>
+                <p className={styles.sectionSubText}>Introduction</p>
+                <h2 className={styles.sectionHeadText}>Overview.</h2>
+            </motion.div>
+            <motion.p variants={fadeIn("", "", 0.1, 1)} className="">
+                I'm a full stack web developer with experience in JavaScript,
+                TypeScript, Python, Java, and PHP, and expertise in frameworks
+                like React, React Native, node.js, Express, Laravel , .NET ,
+                among other tools and frameworks that I've worked with like
+                electron.js, redux state management, Three.js , git and github,
+                postman etc. Let's work together to bring your ideas to life !
+            </motion.p>
+        </>
+    );
 };
 
 export default About;
