282a9aa055b16ba8f5a353ad4b54ecfdb9ef7ecf
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-08-23 22:13:22 +0300

    created animated cards

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,6 +6,35 @@ import { styles } from "../styles";
 import { services } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
 
+const ServiceCard = ({ index, title, icon }) => {
+    return (
+        <Tilt className="xs:w-[250px] w-full">
+            <motion.div
+                variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+                className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card "
+            >
+                <div
+                    options={{
+                        max: 45,
+                        scale: 1,
+                        speed: 450,
+                    }}
+                    className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
+                >
+                    <img
+                        src={icon}
+                        alt={title}
+                        className="w-16 h-16 object-contain"
+                    />
+                    <h3 className="text-white text-[20px] font-bold text-center">
+                        {title}
+                    </h3>
+                </div>
+            </motion.div>
+        </Tilt>
+    );
+};
+
 const About = () => {
     return (
         <>
@@ -13,7 +42,10 @@ const About = () => {
                 <p className={styles.sectionSubText}>Introduction</p>
                 <h2 className={styles.sectionHeadText}>Overview.</h2>
             </motion.div>
-            <motion.p variants={fadeIn("", "", 0.1, 1)} className="">
+            <motion.p
+                variants={fadeIn("", "", 0.1, 1)}
+                className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+            >
                 I'm a full stack web developer with experience in JavaScript,
                 TypeScript, Python, Java, and PHP, and expertise in frameworks
                 like React, React Native, node.js, Express, Laravel , .NET ,
@@ -21,6 +53,15 @@ const About = () => {
                 electron.js, redux state management, Three.js , git and github,
                 postman etc. Let's work together to bring your ideas to life !
             </motion.p>
+            <div className="mt-20 flex flex-wrap gap-10 ">
+                {services.map((service, index) => (
+                    <ServiceCard
+                        key={service.title}
+                        index={index}
+                        {...service}
+                    />
+                ))}
+            </div>
         </>
     );
 };
