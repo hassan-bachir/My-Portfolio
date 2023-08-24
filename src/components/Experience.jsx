298c22fecffa3e1c415a1e9cb93a3bbde0d25fb3
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-08-24 23:27:04 +0300

    finished experience section

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -40,7 +40,12 @@ const ExperienceCard = ({ experience }) => (
         </div>
         <ul className="mt-5 list-disc ml-5 space-y-2">
             {experience.points.map((point, index) => (
-                <li key={"experience "}></li>
+                <li
+                    key={`experience-point-${index}`}
+                    className="text-white-100 text-[14px] pl-1 tracking-wider"
+                >
+                    {point}
+                </li>
             ))}
         </ul>
     </VerticalTimelineElement>
