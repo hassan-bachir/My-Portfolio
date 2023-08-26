dbd42a53aca9a7f873b05e93878eecdf61147203
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-08-27 00:43:47 +0300

    found a bug :)

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -3,23 +3,22 @@ import { Canvas } from "@react-three/fiber";
 import {
     Decal,
     Float,
-    Icosahedron,
     OrbitControls,
     Preload,
     useTexture,
 } from "@react-three/drei";
 
 import CanvasLoader from "../Loader";
-import { AmbientLight, IcosahedronGeometry } from "three";
 
 const Ball = (props) => {
     const [decal] = useTexture([props.imgUrl]);
     return (
         <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
-            <AmbientLight intensity={0.25} />
+            <ambientLight intensity={0.25} />
             <directionalLight position={[0, 0, 0.05]} />
             <mesh castShadow receiveShadow scale={2.75}>
-                <IcosahedronGeometry args={[1, 1]} />
+                <icosahedronGeometry args={[1, 1]} />
+                <meshStandardMaterial />
             </mesh>
         </Float>
     );
