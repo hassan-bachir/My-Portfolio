ddb31f2ac06b91c624ad0cfb3988491a4554cc84
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-08-26 15:34:05 +0300

    created a new section

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import {
+    Decal,
+    Float,
+    Icosahedron,
+    OrbitControls,
+    Preload,
+    useTexture,
+} from "@react-three/drei";
 
-const Ball = () => {
-  return (
-    <div>Ball</div>
-  )
-}
+import CanvasLoader from "../Loader";
+import { AmbientLight, IcosahedronGeometry } from "three";
 
-export default Ball
+const Ball = (props) => {
+    const [decal] = useTexture([props.imgUrl]);
+    return (
+        <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
+            <AmbientLight intensity={0.25} />
+            <directionalLight position={[0, 0, 0.05]} />
+            <mesh castShadow receiveShadow scale={2.75}>
+                <IcosahedronGeometry args={[1, 1]} />
+            </mesh>
+        </Float>
+    );
+};
+const BallCanvas = ({ icon }) => {
+    return (
+        <Canvas frameloop="demand" gl={{ preserveDrawingBuffer: true }}>
+            <Suspense fallback={<CanvasLoader />}>
+                <OrbitControls enableZoom={false} />
+                <Ball imgUrl={icon} />
+            </Suspense>
+
+            <Preload all />
+        </Canvas>
+    );
+};
+export default BallCanvas;
