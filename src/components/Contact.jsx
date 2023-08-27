8a7297f3478d7a685ecf9cad7f64c44da820734f
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-08-27 20:18:29 +0300

    added the navigation to contact section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,7 +7,22 @@ import { SectionWrapper } from "../hoc";
 import { slideIn } from "../utils/motion";
 
 const Contact = () => {
-    return <div>Contact</div>;
+    const formRef = useRef();
+    const [form, setForm] = useState({
+        name: "",
+        email: "",
+        message: "",
+    });
+
+    const [loading, setLoading] = useState(false);
+
+    const handleChange = (e) => {};
+
+    const handleSubmit = (e) => {};
+
+    return (
+        <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden"></div>
+    );
 };
 
-export default Contact;
+export default SectionWrapper(Contact, "contact");
