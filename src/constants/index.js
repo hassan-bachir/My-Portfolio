50825f7b46d12b7d7d6d8ad0847df12366e41026
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-09-18 00:48:57 +0300

    Editednsomethings

--- src/constants/index.js
+++ src/constants/index.js
@@ -140,16 +140,13 @@ const experiences = [
         ],
     },
     {
-        title: "Web Developer",
-        company_name: "Shopify",
+        title: "Founder and Manager",
+        company_name: "Bachir Auto Keys",
         icon: shopify,
         iconBg: "#383E56",
-        date: "Jan 2022 - Jan 2023",
+        date: "January 2017 – September 2023",
         points: [
-            "Developing and maintaining web applications using React.js and other related technologies.",
-            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-            "Implementing responsive design and ensuring cross-browser compatibility.",
-            "Participating in code reviews and providing constructive feedback to other developers.",
+            "Owned and managed automotive keys and services shop, overseeing all aspects of the business, including customer service, inventory, and team performance, resulting in increased revenue and profitability.",
         ],
     },
     {
