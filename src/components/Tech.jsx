6f06aa4701921264ae9b5f64f114befa2d40e8e0
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-09-18 23:18:42 +0300

    added the tools titles

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -2,16 +2,28 @@ import React from "react";
 import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
+import { motion } from "framer-motion";
+import { textVariant } from "../utils/motion";
+import { styles } from "../styles";
 
 const Tech = () => {
     return (
-        <div className="flex flex-row flex-wrap justify-center gap-10">
-            {technologies.map((technology) => (
-                <div className="w-28  h-28 " key={technology.name}>
-                    <BallCanvas icon={technology.icon} />
-                </div>
-            ))}
-        </div>
+        <>
+            <motion.div variants={textVariant()}>
+                <p className={styles.sectionSubText}>My Tech Arsenal</p>
+                <h2 className={styles.sectionHeadText}>Technologies.</h2>
+            </motion.div>
+            <div className="flex flex-row mt-10 flex-wrap justify-center gap-10">
+                {technologies.map((technology) => (
+                    <div className="w-28  h-28 " key={technology.name}>
+                        <p className="text-center mb-3 text-secondary text-[17px]">
+                            {technology.name}
+                        </p>
+                        <BallCanvas icon={technology.icon} />
+                    </div>
+                ))}
+            </div>
+        </>
     );
 };
 
