cbe45f76860746c61baebfae41158514f4ca3ac8
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-09-23 12:10:53 +0300

    added aditional component

--- src/constants/index.js
+++ src/constants/index.js
@@ -81,10 +81,10 @@ const technologies = [
         name: "TypeScript",
         icon: typescript,
     },
-    {
-        name: "React JS",
-        icon: reactjs,
-    },
+    // {
+    //     name: "React JS",
+    //     icon: reactjs,
+    // },
     {
         name: "Redux Toolkit",
         icon: redux,
